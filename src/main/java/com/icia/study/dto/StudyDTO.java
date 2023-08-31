58cb5a1748e1e63adc9e0adfd52fd35398021f94
ScarsIntoStars <conpana_jj@naver.com>
2023-08-31 18:48:07 +0900

    스프링은 어려웡

---
+++ src/main/java/com/icia/study/dto/StudyDTO.java
@@ -0,0 +1,13 @@
+package com.icia.study.dto;
+
+import lombok.Data;
+
+//@Setter
+//@Getter
+//@ToString
+@Data // GETTER + SETTER + ToString
+public class StudyDTO {
+    private String p1;
+    private String p2;
+    private int p3;
+}
